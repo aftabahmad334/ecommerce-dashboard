04aa6d79646a39b4a27ae92a6a7032fce54dd6d6
aftab ahmad <aftab.ahmad334@gmail.com>
2022-02-06 17:59:24 +0500

    react ecommerce dashboard

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button, button} from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello - Ecommerce Project</h1>
+      <button>Normal Button</button>
+      <Button> bootstrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
